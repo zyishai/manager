52a0808a654986e238f49bc9326c31c8289e9884
Yishai Zehavi <zehaviyishai@gmail.com>
2019-08-20 01:05:20 +0300

    implemented basic model
    - Task class and TaskProps interface.
    - TaskStore class for CRUD-ing tasks.

---
+++ src/store/task-store.ts
@@ -0,0 +1,57 @@
+import { observable, computed, action } from 'mobx';
+import { Task, TaskProps } from './task';
+import { validate } from 'class-validator';
+import { string } from 'prop-types';
+
+export class TaskStore {
+    @observable private _tasks: Task[] = [];
+
+    // @fix: this method creates, validates and add new task (violates single responsibility priciple).
+    @action
+    async addTask({ title, goal, description }: TaskProps) {
+        const task = new Task({
+            title,
+            description,
+            goal
+        });
+
+        const errors = await validate(task);
+        
+        if (errors && errors.length) {
+            console.error(errors);
+            return;
+        }
+
+        this._tasks.push(task);
+    }
+
+    @action
+    async updateTask(id: string, { title, goal, description }: { title?: string, goal?: string,  description?: string }) {
+        const task = this._tasks.find(task => task.id === id);
+
+        if (task) {
+            task.title = title || task.title;
+            task.goal = goal || task.goal;
+            task.description = description || task.description;
+            return true;
+        }
+
+        return false;
+    }
+
+    @action
+    async removeTask(taskId: string) {
+        const taskIndex = this._tasks.findIndex(task => task.id === taskId);
+
+        if (taskIndex >= 0) {
+            return this._tasks.splice(taskIndex, 1);
+        }
+
+        return null;
+    }
+
+    @computed
+    get tasks() {
+        return this._tasks.slice();
+    }
+}
